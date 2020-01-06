05e3f34750ed4e5eb449391d9efe2d74d1fe4308
Xingyu-Lin <abclin6693@gmail.com>
2020-01-06 02:11:44 -0500

    add utils for saving numpy arrays to gif

--- softgym/utils/make_gif.py
+++ softgym/utils/make_gif.py
@@ -3,6 +3,9 @@ import cv2
 import numpy as np
 import imageio
 import glob
+from PIL import Image
+from moviepy.editor import ImageSequenceClip
+
 
 def make_gif(video_path, video_name, video_idx_st, video_idx_en, video_height, video_width, clean_dir=True):
     """
@@ -28,4 +31,40 @@ def make_gif(video_path, video_name, video_idx_st, video_idx_en, video_height, v
         print('Removing all render_*.tga files in {}'.format(video_path))
         for f in glob.glob(os.path.join(video_path, "render_*.tga")):
             os.remove(f)
+
+
+def save_numpy_as_gif(array, filename, fps=10, scale=1.0):
+    """Creates a gif given a stack of images using moviepy
+    Notes
+    -----
+    works with current Github version of moviepy (not the pip version)
+    https://github.com/Zulko/moviepy/commit/d4c9c37bc88261d8ed8b5d9b7c317d13b2cdf62e
+    Usage
+    -----
+    >>> X = randn(100, 64, 64)
+    >>> gif('test.gif', X)
+    Parameters
+    ----------
+    filename : string
+        The filename of the gif to write to
+    array : array_like
+        A numpy array that contains a sequence of images
+    fps : int
+        frames per second (default: 10)
+    scale : float
+        how much to rescale each image by (default: 1.0)
+    """
+
+    # ensure that the file has the .gif extension
+    fname, _ = os.path.splitext(filename)
+    filename = fname + '.gif'
+
+    # copy into the color dimension if the images are black and white
+    if array.ndim == 3:
+        array = array[..., np.newaxis] * np.ones(3)
+
+    # make the moviepy clip
+    clip = ImageSequenceClip(list(array), fps=fps).resize(scale)
+    clip.write_gif(filename, fps=fps)
+    return clip
 
