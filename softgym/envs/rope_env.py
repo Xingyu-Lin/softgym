e9e5ef37b219bea22903bc30b5003f04318cac8e
Xingyu-Lin <abclin6693@gmail.com>
2020-01-19 15:41:53 -0500

    cloth flatten environment with initial states

--- softgym/envs/rope_env.py
+++ softgym/envs/rope_env.py
@@ -11,15 +11,37 @@ class RopeEnv(FlexEnv):
         self.render_mode = render_mode
         super().__init__(**kwargs)
 
-        assert observation_mode in ['key_point', 'point_cloud', 'cam_rgb']
-        assert action_mode in ['key_point_pos', 'key_point_vel', 'sphere', 'picker']
+        assert observation_mode in ['point_cloud', 'cam_rgb']
+        assert action_mode in ['picker']
         self.observation_mode = observation_mode
         self.action_mode = action_mode
+
+        if action_mode == 'picker':
+            self.action_tool = Picker(num_picker, picker_radius=0.1)
+            self.action_space = self.action_tool.action_space
+
+        max_particles = 30
+        if observation_mode == 'point_cloud' and action_mode == 'picker':
+            self.observation_space = Box(np.array([-np.inf] * (max_particles * 3 + num_picker * 3)),
+                                         np.array([np.inf] * (max_particles * 3 + num_picker * 3)), dtype=np.float32)
 
         self.horizon = horizon
 
         self.cached_configs = [self.get_default_config()]
         self.cached_init_states = [None]
+
+    def get_default_config(self):
+        """ Set the default config of the environment and load it to self.config """
+        config = {
+            'RopeLength': 10,
+            'camera_name': 'default_camera',
+            'camera_params': {'default_camera':
+                                  {'pos': np.array([0., 7., 3.]),
+                                   'angle': np.array([0, -65 / 180. * np.pi, 0.]),
+                                   'width': self.camera_width,
+                                   'height': self.camera_height}}
+        }
+        return config
 
     def initialize_camera(self):
         pass
@@ -27,28 +49,10 @@ class RopeEnv(FlexEnv):
     def _reset(self):
         pass
 
-    def get_default_config(self):
-        """ Set the default config of the environment and load it to self.config """
-        config = {
-            'ClothPos': [-1.6, 2.0, -0.8],
-            'ClothSize': [64, 32],
-            'ClothStiff': [0.9, 1.0, 0.9],  # Stretch, Bend and Shear
-            'camera_name': 'default_camera',
-            'camera_params': {'default_camera':
-                                  {'pos': np.array([0., 4., 0.]),
-                                   'angle': np.array([0, -70 / 180. * np.pi, 0.]),
-                                   'width': self.camera_width,
-                                   'height': self.camera_height}}
-        }
-        return config
-
-    def _get_obs(self):  # NOTE: just rename to _get_obs
+    def _get_obs(self):
         particle_pos = np.array(pyflex.get_positions()).reshape([-1, 4])[:, :3]
-        keypoint_pos = particle_pos[self._get_obs_key_point_idx(), :3]
         if self.observation_mode == 'point_cloud':
             pos = particle_pos
-        elif self.observation_mode == 'key_point':
-            pos = keypoint_pos
         elif self.observation_mode == 'cam_rgb':
             return self.render().flatten()
 
@@ -86,28 +90,28 @@ class RopeEnv(FlexEnv):
         elif self.render_mode == 'both':
             render_mode = 3
         camera_params = config['camera_params'][config['camera_name']]
-        params = np.array([*config['ClothPos'], *config['ClothSize'], *config['ClothStiff'], render_mode,
-                           *camera_params['pos'][:], *camera_params['angle'][:], camera_params['width'], camera_params['height']])
-        self.params = params  # YF NOTE: need to save the params for sampling goals
+        # params = np.array([*config['ClothPos'], *config['ClothSize'], *config['ClothStiff'], render_mode,
+        #                    *camera_params['pos'][:], *camera_params['angle'][:], camera_params['width'], camera_params['height']])
+        # self.params = params  # YF NOTE: need to save the params for sampling goals
         pyflex.set_scene(12, [], 0)
-        # if state is not None:
-        #     self.set_state(state)
+        if state is not None:
+            self.set_state(state)
         self.current_config = deepcopy(config)
-
-    # def get_state(self):
-    #     # TODO: Xingyu: fix this before running CEM
-    #     cur_state = super().get_state()
-    #     return cur_state
-    #
-    # def set_state(self, state_dict):
-    #     super().set_state(state_dict)
 
     def _get_info(self):
         return {}
 
+    def _get_center_point(self, pos):
+        pos = np.reshape(pos, [-1, 4])
+        min_x = np.min(pos[:, 0])
+        min_y = np.min(pos[:, 2])
+        max_x = np.max(pos[:, 0])
+        max_y = np.max(pos[:, 2])
+        return 0.5 * (min_x + max_x), 0.5 * (min_y + max_y)
+
 
 if __name__ == '__main__':
-    env = RopeEnv(observation_mode='key_point',
+    env = RopeEnv(observation_mode='point_cloud',
                   action_mode='picker',
                   num_picker=2,
                   render=True,
